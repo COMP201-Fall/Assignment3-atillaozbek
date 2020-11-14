8bac389508205c486fbd65f281f761fb1ca082b2
Muhammad Aditya Sasongko <msasongko17@ku.edu.tr>
2020-11-14 21:50:09 +0300

    Initial commit

---
+++ floyd_warshall.c
@@ -0,0 +1,176 @@
+/* This is a code for COMP 201 assignment 3.
+ * In this code, Floyd-Warshall algorithm is implemented
+ * to find the shortest paths between any two cities
+ * in a given graph of cities.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const int INF = 1e7;  
+
+/* Array of vertices. 
+ * Each element of the str array contains a city name and 
+ * its index in str becomes the city's numeric id.
+ */
+typedef struct vertices 
+{
+	// This attribute shows the size of dynamically allocated memory
+	int size;
+	// This attribute shows the number of cities in the array
+	int city_count;
+	// This attribute is the array of city names
+	char (* str)[30];
+} 
+vertices;
+
+/* Array of edges. 
+ * Each element of the edge array contains the ids of two cities 
+ * connected directy by a road and the length of the road.
+ */
+typedef struct edges 
+{
+	// This attribute shows the size of dynamically allocated memory
+	int size;
+	// This attribute shows the number of roads in the array
+	int edge_count;
+	// This attribute is the array of road information
+	int (* edge)[3];
+} 
+edges;
+
+vertices cities;
+
+edges roads;
+
+/* A two-dimensional array that shows 
+ * the length of the shortest path connecting 
+ * any two cities.
+ */
+int **city_graph;
+
+/* A two-dimensional array that shows the direction 
+ * to the shortest path between any two cities.
+ */
+int **shortest_paths;
+
+/* A function that implements Floyd-Warshall algorithm. 
+ * This algorithm finds the shortest path between 
+ * any two vertices in a given graph.
+ */
+void floydWarshall()  
+{  
+	for (int k = 0; k < cities.city_count; k++) 
+	{  
+		for (int i = 0; i < cities.city_count; i++) 
+		{  
+			for (int j = 0; j < cities.city_count; j++) 
+			{  
+
+				// We cannot cross a road that doesn't exist  
+				if ((city_graph[i][k] == INF) || (city_graph[k][j] == INF))  
+				{
+					continue;
+				}  
+
+				if (city_graph[i][j] > (city_graph[i][k] + city_graph[k][j])) 
+				{  
+					city_graph[i][j] = city_graph[i][k] + city_graph[k][j];  
+					shortest_paths[i][j] = shortest_paths[i][k];  
+				}  
+			}  
+		}  
+	}  
+} 
+
+/* A function that initializes the values in 
+ * city_graph and shortest_paths arrays.
+ */
+void initialise()  
+{  
+	for (int i = 0; i < cities.city_count; i++) 
+	{  
+		for (int j = 0; j < cities.city_count; j++) 
+		{    
+
+			// No edge between node i and j  
+			if (city_graph[i][j] == INF)  
+			{
+				shortest_paths[i][j] = -1;
+			}  
+			else
+			{
+				shortest_paths[i][j] = j;
+			}  
+		}  
+	}  
+}
+
+/* A function that inserts the name of a new city 
+ * to the cities array.
+ */
+int insert_to_cities(char str[30]) 
+{
+	// Write your code here
+	// Check if the city name already exists in the array 
+	// If it does, return the array index of the city 
+
+	// If the city name doesn't exist, it is inserted to the vertex array, and
+	// return the array index of the city
+
+	// If the dynamically allocated size of vertex_array is not large enough, 
+	// vertex_array is enlarged and the city name is inserted 
+	// to the newly added region
+
+	// Return the array index of the city.
+}
+
+/* A function that looks up if a city name
+ * is already in the cities array. 
+ */
+int lookup_string(char str[30]) 
+{
+	for(int i = 0; i < cities.city_count; i++) {
+		if(strncmp(str, cities.str[i], 30) == 0)
+			return i;
+	}
+	printf("city %s is not found\n", str);
+	return -1;
+}
+
+void printPath(int u, int v)  
+{  
+	// Write your code here  
+} 
+
+int main(int argc, char *argv[])
+{
+	// Write your code here
+
+	// Allocate memory regions dynamically to vertex array 
+	// and edge_array.
+
+	// Read and parse the input file. Insert the city names 
+	// and ids to vertex_array. 
+	// Insert city ids and road lengths to edge_array.
+
+	// Allocate memory regions dynamically to city_graph, 
+	// distance, and shortest_paths arrays.
+
+	// Initialize the values in city_graph array with road lengths, 
+	// such that the value in city_graph[i][j] is the road length 
+	// between cities i and j if these cities are directly connected 
+	// by a road 
+	// For cities m and n that are not connected directly by a road, 
+	// the value in city_graph[m][n] will be INF, 
+    // which is a large value like 10M, 
+	// that is assumed to be infinite. 
+	initialise();
+	floydWarshall();
+	while(1) {
+		// prompt user to enter two city names
+		// print the shortest path between the two cities
+		// print the length of the path
+	}
+	return 0;	
+}
